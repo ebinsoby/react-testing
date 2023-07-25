ac42d58313480b942f11f8c11b38120b3bba5694
Ebin Soby <mailebinsoby@gmail.com>
2023-07-25 13:48:18 -0400

    adding in multiple file types aligning with react conventions

---
+++ src/__tests__/greet-test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import Greet from "../components/greet/greet";
+
+describe("Greet", () => {
+  test("Greet renders correctly", () => {
+    render(<Greet name={undefined} />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
+  describe("Nested describe", () => {
+    test("Greet renders a name", () => {
+      render(<Greet name="Ebin" />);
+      const textElement = screen.getByText("Hello Ebin");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
+});
