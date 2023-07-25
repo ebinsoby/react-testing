ff5a7f92844c7cc0d11b6fa498662d5107219b14
esoby <ebin.soby@alertdriving.com>
2023-07-25 11:49:10 -0400

    adding in test filters

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -2,7 +2,13 @@ import { render, screen } from "@testing-library/react";
 import Greet from "./greet";
 
 test("Greet renders correctly", () => {
-  render(<Greet />);
+  render(<Greet name={undefined} />);
   const textElement = screen.getByText(/hello/i);
   expect(textElement).toBeInTheDocument();
 });
+
+test.skip("Greet renders a name", ()=>{
+  render(<Greet name='Ebin'/>);
+  const textElement = screen.getByText('Hello Ebin')
+  expect(textElement).toBeInTheDocument();
+})
--- src/components/greet/greet.tsx
+++ src/components/greet/greet.tsx
@@ -1,5 +1,7 @@
-export default function greet() {
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
+
+export default function greet(props: { name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }) {
   return (
-    <div>Hello</div>
+    <div>Hello {props.name}</div>
   )
 }
