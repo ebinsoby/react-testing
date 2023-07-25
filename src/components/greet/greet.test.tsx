88fe7d6178370a162da536cb4b54d5c95d38f850
esoby <ebin.soby@alertdriving.com>
2023-07-25 11:54:09 -0400

    grouping test, describe, nested describe

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet name={undefined} />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("Greet renders correctly", () => {
+    render(<Greet name={undefined} />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
+  describe("Nested describe", () => {
+    test("Greet renders a name", () => {
+      render(<Greet name="Ebin" />);
+      const textElement = screen.getByText("Hello Ebin");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
 });
-
-test.skip("Greet renders a name", ()=>{
-  render(<Greet name='Ebin'/>);
-  const textElement = screen.getByText('Hello Ebin')
-  expect(textElement).toBeInTheDocument();
-})
